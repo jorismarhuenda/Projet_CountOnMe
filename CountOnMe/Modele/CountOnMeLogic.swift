1b340db26fd6662af01f6e12d3b3302b8e8c0164
Joris Marhuenda <marhuendajoris@gmail.com>
2021-07-25 18:32:09 +0200

    Ajout des propriété de la classe CountOnMeLogic ainsi que des protocoles pour mettre à jour l'affichage

--- CountOnMe/Modele/CountOnMeLogic.swift
+++ CountOnMe/Modele/CountOnMeLogic.swift
@@ -9,4 +9,45 @@
 import Foundation
 import UIKit
 
+protocol CountOnMeDelegate {
+    func alertShow(title: String, message: String)
+    func updateTextView(label: String)
+}
 
+class CountOnMeLogic {
+    //Array of numbers.
+    var stringNumber: [String] = [String()]
+    
+    //Array of operators.
+    var operators: [String] = ["+"]
+    var index = 0
+    
+    //Var that holds the delegate.
+    var countOnMeDelegate: CountOnMeDelegate?
+    
+    //Var checking if the expression is correctly typed by the user, else it will alert the user.
+    var isExpressionIsCorrect: Bool {
+        if let stringNumber = stringNumber.last {
+            if stringNumber.isEmpty {
+                if stringNumber.count == 1 {
+                    countOnMeDelegate?.alertShow(title: "Zero", message: "Start new calculation")
+                } else {
+                    countOnMeDelegate?.alertShow(title: "Zero", message: "Type correct expression please")
+                }
+                return false
+            }
+        }
+        return true
+    }
+    
+    //Var checking if a string of numbers contains something, else it will alert the user to type a string of numbers first and then the user can add an operator.
+    var canAddOperator: Bool {
+        if let stringNumber = stringNumber.last {
+            if stringNumber.isEmpty {
+                countOnMeDelegate?.alertShow(title: "Zero!", message: "Wrong expression!")
+                return false
+            }
+        }
+        return true
+    }
+}
