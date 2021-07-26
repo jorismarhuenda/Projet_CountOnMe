8557aa76ed03642b570a4a8f8543979af9127a7f
Joris Marhuenda <marhuendajoris@gmail.com>
2021-07-26 14:26:59 +0200

    Ajout des méthodes de la classe CountOnMeLogic qui vont servir à réaliser les différents calculs

--- CountOnMe/Modele/CountOnMeLogic.swift
+++ CountOnMe/Modele/CountOnMeLogic.swift
@@ -9,24 +9,21 @@
 import Foundation
 import UIKit
 
-//Using protocol to delegate alerts and update the display.
+// Using protocol to delegate alerts and update the display.
 protocol CountOnMeDelegate {
     func alertShow(title: String, message: String)
     func updateTextView(label: String)
 }
 
 class CountOnMeLogic {
-    //Array of numbers.
+    // Array of numbers.
     var stringNumber: [String] = [String()]
-    
-    //Array of operators.
+    // Array of operators.
     var operators: [String] = ["+"]
     var index = 0
-    
-    //Var that holds the delegate.
+    // Var that holds the delegate.
     var countOnMeDelegate: CountOnMeDelegate?
-    
-    //Var checking if the expression is correctly typed by the user, else it will alert the user.
+    // Var checking if the expression is correctly typed by the user, else it will alert the user.
     var isExpressionIsCorrect: Bool {
         if let stringNumber = stringNumber.last {
             if stringNumber.isEmpty {
@@ -40,8 +37,7 @@ class CountOnMeLogic {
         }
         return true
     }
-    
-    //Var checking if a string of numbers contains something, else it will alert the user to type a string of numbers first and then the user can add an operator.
+    // Var checking if a string of numbers contains something.
     var canAddOperator: Bool {
         if let stringNumber = stringNumber.last {
             if stringNumber.isEmpty {
@@ -50,5 +46,59 @@ class CountOnMeLogic {
             }
         }
         return true
+    }
+    // Method managing numbers when user types them.
+    func addNewNumber(_ newNumber: Int) {
+        if let stringNumber = stringNumber.last {
+            var stringNumberMutable = stringNumber
+            stringNumberMutable += "\(newNumber)"
+            stringNumber[stringNumber.count-1] = stringNumberMutable
+        }
+        updateLabelText()
+    }
+    // Method managing the divide operator when user types it.
+    func divide() {
+        if canAddOperator {
+            operators.append("/")
+            stringNumber.append("")
+            updateLabelText()
+        }
+    }
+    // Method managing the multiply operator when user types it.
+    func multiply() {
+        if canAddOperator {
+            operators.append("X")
+            stringNumber.append("")
+            updateLabelText()
+        }
+    }
+    // Method managing the minus operator when users types it
+    func minus() {
+        if canAddOperator {
+            operators.append("-")
+            stringNumber.append("")
+            updateLabelText()
+        }
+    }
+    // Method managing the plus operator when users types it
+    func plus() {
+        if canAddOperator {
+            operators.append("+")
+            stringNumber.append("")
+            updateLabelText()
+        }
+    }
+    // Method managing to update the label text.
+    func updateLabelText() {
+        var text = ""
+        for (index, stringNumber) in stringNumber.enumerated() {
+            // Add operator
+            if index > 0 {
+                text += operators[index]
+            }
+            // Add number
+            text += stringNumber
+        }
+        countOnMeDelegate?.updateTextView(label: text)
     }
 }
